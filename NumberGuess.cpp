3da09300452316db4da7e0cfb5b94d105f1e095b
AliHaider332 <bhaialihaider332@gmail.com>
2025-03-05 22:05:41 +0500

    NumberGuess.cpp

---
+++ NumberGuess.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int number=rand()%10+1;
+    int x;
+    int iteration=0;
+    cout<<" Welcome to the Number Guessing Game! "<<endl;
+    cout<<"|\n|\n|\n"<<"Guess a number between 1 and 1000: \n"<<"|\n|\n|\n"<<endl;
+
+    while(true)
+    {
+        cout<<"Enter your guess: ";
+        cin>>x;
+        iteration++;
+        if(x==0)
+        {
+            cout<<"OOP'S you failed to guess the number: "<<endl;
+            break;
+        }
+        if(x==number)
+        {
+            cout<<"Congratulations! You guessed the number in "<<iteration<<" attempts.";
+            break;
+        }
+        else if(x>number)
+        {
+            cout<<"Too Long! try again: "<<endl;
+        }
+        else if (x<number)
+        {
+            cout<<"Too Short! try again: "<<endl;
+        }
+        cout<<"If you want to quit enter 0 in guess: "<<endl;
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# 🎯 Number Guessing Game in C++
+
+This is a simple **Number Guessing Game** built using **C++**. The game generates a **random number** between **1 and 10** (you can modify the range), and the player has to guess it!
+
+## 🚀 Features  
+- 🎲 **Random number generation** using `rand()`  
+- 🕹️ **Interactive gameplay** with hints (`Too Long!` / `Too Short!`)  
+- 🔄 **Unlimited attempts** until the correct number is guessed  
+- ❌ **Exit option** by entering `0`  
+
+## 📂 How to Run  
+
+1. **Clone the repository:**  
+   ```bash
+   git clone https://github.com/your-username/Number-Guessing-Game.git
